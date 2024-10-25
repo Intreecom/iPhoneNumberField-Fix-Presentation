a6c159d77f2375404ab9ef3e74b40639f162b2d4
Johan Albrectsen <johan@albrectsen.com>
2024-10-25 14:04:49 +0200

    Solved phone number kit reference.

--- Package.swift
+++ Package.swift
@@ -16,13 +16,16 @@ let package = Package(
     ],
     dependencies: [
         // Dependencies declare other packages that this package depends on.
-        .package(url: "https://github.com/marmelroy/PhoneNumberKit", .upToNextMajor(from: "3.4.5"))
+        .package(url: "https://github.com/Intreecom/PhoneNumberKit-Fix-Presentation", from: Version(1, 0, 0))
     ],
     targets: [
         // Targets are the basic building blocks of a package. A target can define a module or a test suite.
         // Targets can depend on other targets in this package, and on products in packages this package depends on.
         .target(
             name: "iPhoneNumberField",
-            dependencies: ["PhoneNumberKit"]),
+            dependencies: [
+                .product(name: "PhoneNumberKit", package: "PhoneNumberKit-Fix-Presentation")
+            ]
+        ),
     ]
 )
